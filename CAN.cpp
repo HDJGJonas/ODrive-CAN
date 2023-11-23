6a0a57d21ddd150c915e0428a324a56337ec8341
HDJGJonas <jonas.haeberli@hispeed.ch>
2023-11-23 21:53:41 +0100

    Improved CAN reading

--- CAN.cpp
+++ CAN.cpp
@@ -1,9 +1,21 @@
 #include "CAN.h"
 #include <iostream>
+#include <map>
+#include <thread>
+#include <chrono>
+
+struct TimedFrame {
+    can_frame frame;
+    std::chrono::steady_clock::time_point timestamp;
+};
+
+std::map<uint32_t, TimedFrame> latestFrames;
+
+can_frame errorFrame = {0, 0, 0, {0x99, 0x99, 0x99, 0x99, 0, 0, 0, 0}};
 
 ODrive::CAN::CAN() {
     struct sockaddr_can Address;
-	struct ifreq IFReq;
+    struct ifreq IFReq;
 
     // Socket setup.
     this->SocketFD = socket(PF_CAN, SOCK_RAW, CAN_RAW);
@@ -24,6 +36,10 @@ ODrive::CAN::CAN() {
         // Error; throw exception.
         exit(1);
     }
+
+    // Start the read loop in a separate thread.
+    std::thread readThread(&ODrive::CAN::ReadLoop, this);
+    readThread.detach();
 }
 
 ODrive::CAN::~CAN() {
@@ -31,41 +47,42 @@ ODrive::CAN::~CAN() {
     close(this->SocketFD);
 }
 
-// ssize_t ODrive::CAN::Read(can_frame *Frame) {
-//     return read(this->SocketFD, Frame, sizeof(can_frame));
-// }
-
 ssize_t ODrive::CAN::Write(can_frame *Frame) {
     return write(this->SocketFD, Frame, sizeof(can_frame));
 }
 
-ssize_t ODrive::CAN::Read(can_frame *Frame) {
-    ssize_t bytesRead;
-    uint32_t desiredCanId = Frame -> can_id;
-    int remainingTimeout = 10000000; //microseconds
-    
-    while (remainingTimeout > 0) {
-        bytesRead = read(this->SocketFD, Frame, sizeof(can_frame));
-        
+void ODrive::CAN::ReadLoop() {
+    can_frame Frame;
+    while (true) {
+        ssize_t bytesRead = read(this->SocketFD, &Frame, sizeof(can_frame));
+
         if (bytesRead == -1) {
             // Handle read error, e.g., print an error message or return an error code.
-            return -1;
-        } else if (bytesRead == sizeof(can_frame) && Frame->can_id == desiredCanId) {
-            // Successfully read a frame with the desired CAN ID.
-            return bytesRead;
+            continue;
+        } else if (bytesRead == sizeof(can_frame)) {
+            // Successfully read a frame. Update the map with the new frame and the current time.
+            latestFrames[Frame.can_id] = {Frame, std::chrono::steady_clock::now()};
         }
-        
-        // Update the remaining timeout.
-        remainingTimeout -= 10000; // 10,000 microseconds = 0.01 seconds
-        
-        // Sleep for a short time to avoid busy-waiting.
-        usleep(10000); // 10,000 microseconds = 0.01 seconds
     }
-    // Return an axis error if timeout is hit axis error is 0x99999999 32bit
-    Frame->data[0] = 0x99;
-    Frame->data[1] = 0x99;
-    Frame->data[2] = 0x99;
-    Frame->data[3] = 0x99;
+}
 
-    return 4;
+ssize_t ODrive::CAN::Read(can_frame *Frame) {
+    uint32_t desiredCanId = Frame->can_id;
+
+    if (latestFrames.count(desiredCanId) > 0) {
+        // If we have a frame with the desired CAN ID, check its timestamp.
+        auto now = std::chrono::steady_clock::now();
+        auto frameTime = latestFrames[desiredCanId].timestamp;
+        auto age = std::chrono::duration_cast<std::chrono::milliseconds>(now - frameTime).count();
+
+        if (age <= 200) {
+            // If the frame is not older than 200ms, copy its data to the input frame.
+            *Frame = latestFrames[desiredCanId].frame;
+            return sizeof(can_frame);
+        }
+    }
+
+    // If we don't have a frame with the desired CAN ID, or if the frame is too old, return the error frame.
+    *Frame = errorFrame;
+    return sizeof(can_frame);
 }
