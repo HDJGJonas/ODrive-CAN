f8881c514e862a45f0026bfe77f73ef20a444021
HDJGJonas <jonas.haeberli@hispeed.ch>
2023-12-19 18:22:32 +0100

    Small changes to try robot arm

--- main.cpp
+++ main.cpp
@@ -5,43 +5,84 @@
 #include <iostream>
 #include <cmath>
 
-void generateSinusWave(float startingValue1, float startingValue2, float amplitude, float frequency, float* waveform1, float* waveform2, int numSamples) {
-    for (int i = 0; i < numSamples; i++) {
-        float position1 = startingValue1 + amplitude * std::sin(2 * M_PI * frequency * i / numSamples);
-        float position2 = startingValue2 + amplitude * std::sin(2 * M_PI * frequency * i / numSamples);
-        waveform1[i] = position1;
-        waveform2[i] = position2;
-    }
-}
+#include<unistd.h>
 
-int main (int argc, char *argv[]) { 
-    ODrive::ODrive Hndl;
-    union {
+void moveJ(ODrive::ODrive& Hndl, float J1, float J2, float J3, float J4, float J5, float J6)
+{
+    union
+    {
         float f;
         uint32_t u;
-    }punning;
+    } punning;
 
-    // Generata a sinusoidal waveform
-    const int numSamples = 2000;
-    float waveform1[numSamples];
-    float waveform2[numSamples];
-    float startingValue1 = 0.0f;
-    float startingValue2 = 0.0f;
-    float amplitude = 5.0f;
-    float frequency = 1.0f;
+    punning.f = 6.0+J1;
+    Hndl.SetInputPos(1, punning.u, 0, 0);
 
-    generateSinusWave(startingValue, amplitude, frequency, waveform1, waveform2, numSamples);
+    punning.f = J2;
+    Hndl.SetInputPos(2, punning.u, 0, 0);
 
-    // Use the generated waveforms for motor control
+    punning.f = (J3+25)*(-1);
+    Hndl.SetInputPos(3, punning.u, 0, 0);
 
-    while (true) {
-        for (int i = 0; i < numSamples; i++) {
-            punning.f = waveform1[i];
-            Hndl.SetPosition(1, punning.u, 0, 0);
-            punning.f = waveform2[i];
-            Hndl.SetPosition(2, punning.u, 0, 0);
-            std::cout << "Position 1: " << waveform1[i] << " Position 2: " << waveform2[i] << std::endl;
-        }
+    punning.f = J4;
+    Hndl.SetInputPos(4, punning.u, 0, 0);
+
+    punning.f = J5;
+    Hndl.SetInputPos(5, punning.u, 0, 0);
+
+    punning.f = J6;
+    Hndl.SetInputPos(6, punning.u, 0, 0);
+}
+
+void home(ODrive::ODrive& Hndl){
+    union
+    {
+        float f;
+        uint32_t u;
+    } punning;
+
+    punning.f = 6.0;
+    Hndl.SetInputPos(1, punning.u, 0, 0);
+
+    punning.f = 0.0;
+    Hndl.SetInputPos(2, punning.u, 0, 0);
+
+    punning.f = 0.0;
+    Hndl.SetInputPos(3, punning.u, 0, 0);
+
+    punning.f = 0.0;
+    Hndl.SetInputPos(4, punning.u, 0, 0);
+
+    punning.f = 0.0;
+    Hndl.SetInputPos(5, punning.u, 0, 0);
+
+    punning.f = 0.0;
+    Hndl.SetInputPos(6, punning.u, 0, 0);
+}
+
+int main(int argc, char *argv[])
+{
+    ODrive::ODrive Hndl;
+    // home(Hndl);
+    // usleep(10 * 1000000);//sleeps for 3 second
+    moveJ(Hndl,0,0,0,0,0,0);
+
+    while (true){
+    usleep(4 * 1000000);//sleeps for 3 second
+
+    moveJ(Hndl,8.682,7.998,6.366,10.476,-16.155,-6.187);
+
+    usleep(4 * 1000000);//sleeps for 3 second
+
+    moveJ(Hndl,-8.472,8.261,5.897,-10.326,-15.872,6.212);
+
+    usleep(4 * 1000000);//sleeps for 3 second
+
+    moveJ(Hndl,-8.682,1.631,-0.896,-23.791,-8.710,23.585);
+
+    usleep(4 * 1000000);//sleeps for 3 second
+
+    moveJ(Hndl,8.682,1.631,-0.896,-26.208,8.710,26.414);
     }
 
     return 0;
